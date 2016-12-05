de9fe979c77adf3bf6aec02ebfffeaf54a243b94
Adam Nalisnick <theadam4257@gmail.com>
2016-12-04 23:06:36 -0500

    Enhance clean install

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # dotfiles
+
+## Installation
+
+```
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install git
+git clone https://github.com/theadam/dotfiles.git
+cd dotfiles
+./install.sh
+```
--- install/brew.sh
+++ install/brew.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
 echo 'Installing homebrew'
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap caskroom/cask
 brew tap caskroom/versions
 
 echo 'Installing necessary applications'
-brew install git rbenv nvm bash-completion python3 boost homebrew/completions/tmuxinator-completion
+brew install rbenv nvm bash-completion python3 boost homebrew/completions/tmuxinator-completion
 brew cask install flux bettertouchtool docker slack licecap iterm2 google-chrome
 
 brew install neovim/neovim/neovim
