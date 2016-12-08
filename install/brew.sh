e2092fe3cfb359e48565f963898696af940f8bbf
Adam Nalisnick <theadam@Bert.local>
2016-12-08 15:14:10 -0500

    Add gcc

--- install/brew.sh
+++ install/brew.sh
@@ -4,7 +4,7 @@ brew tap caskroom/cask
 brew tap caskroom/versions
 
 echo 'Installing necessary applications'
-brew install rbenv nvm bash-completion python3 boost tmux homebrew/completions/tmuxinator-completion reattach-to-user-namespace ag
+brew install gcc rbenv nvm bash-completion python3 boost tmux homebrew/completions/tmuxinator-completion reattach-to-user-namespace ag
 brew cask install flux bettertouchtool docker slack licecap iterm2 google-chrome dropbox
 
 brew install neovim/neovim/neovim
