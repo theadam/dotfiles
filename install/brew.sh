1cce89b19029951e82f93102ca066d7b082bf949
Adam Nalisnick <theadam4257@gmail.com>
2018-12-29 00:53:46 -0500

    Fix zsh_profile java home and install oh-my-zsh

--- install/brew.sh
+++ install/brew.sh
@@ -4,6 +4,10 @@ brew tap caskroom/cask
 brew tap caskroom/versions
 
 echo 'Installing necessary applications'
-brew install rbenv nvm bash-completion boost tmux tmuxinator-completion reattach-to-user-namespace ag fzf pyenv pipenv pyenv-virtualenv neovim stow
+brew install coreutils rbenv nvm bash-completion boost tmux tmuxinator-completion reattach-to-user-namespace ag fzf pyenv pipenv pyenv-virtualenv neovim stow zsh
 
 brew cask install flux bettertouchtool docker slack licecap google-chrome dropbox alacritty karabiner-elements
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Remove zshrc installed by oh-my-zsh
+rm ~/.zshrc
