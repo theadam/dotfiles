01f0b86d48342ac71d15b30a6861bd34a231ba04
Adam Nalisnick <theadam4257@gmail.com>
2018-12-28 23:09:09 -0500

    Add newline

--- install/brew.sh
+++ install/brew.sh
@@ -5,6 +5,7 @@ brew tap caskroom/versions
 
 echo 'Installing necessary applications'
 brew install rbenv nvm bash-completion boost tmux tmuxinator-completion reattach-to-user-namespace ag fzf pyenv pipenv pyenv-virtualenv neovim stow
+
 brew cask install flux bettertouchtool docker slack licecap google-chrome dropbox alacritty
 
 /usr/local/opt/fzf/install --all
