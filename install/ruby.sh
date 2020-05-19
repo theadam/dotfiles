c372eb506f0d1c410ca5df0718af39066a5bf38b
Adam Nalisnick <theadam4257@gmail.com>
2020-05-19 12:12:59 -0400

    Update ruby

--- install/ruby.sh
+++ install/ruby.sh
@@ -3,8 +3,8 @@ echo 'Setting up rbenv'
 eval "$(rbenv init -)"
 
 echo 'Installing ruby'
-rbenv install -s 2.3.3
-rbenv global 2.3.3
+rbenv install -s 2.7.1
+rbenv global 2.7.1
 
 echo 'Installing global gems'
 gem install bundler tmuxinator
